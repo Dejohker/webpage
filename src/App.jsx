56e3441206c1d31e210dbcf0eb0d0ec01cda8138
Deola <adeolarazaq00@gmail.com>
2026-06-24 04:36:40 -0700

    Add greeting component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,13 @@
 import Welcome from "./Welcome";
+import Greeting from "./Greeting";
 
-const Joker = () => {
+function App() {
   return (
     <div>
-      <Welcome text={"Ganiyu"} />
-      <Welcome text={"Alameen"} />
+      <Welcome text={"to React"} />
+      <Greeting name={"Joker"} />
     </div>
-    // <div>
-    //   <div className="login-container">
-    //     <h1>Welcome Back</h1>
-    //     <p>Sign in to continue</p>
-
-    //     <form>
-    //       <input type="email" placeholder="Email Address" required />
-
-    //       <input type="password" placeholder="Password" required />
-
-    //       <button type="submit">Login</button>
-    //     </form>
-
-    //     <a href="#">Forgot Password?</a>
-    //   </div>
-    // </div>
   );
-};
+}
 
-export default Joker;
+export default App;
